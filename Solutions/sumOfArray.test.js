e5e230dbdc4d8d5bbc52f00f9372149bc663bc07
AyeshaMalik-stack <77988816+AyeshaMalik-stack@users.noreply.github.com>
2023-02-13 14:50:44 +0100

    Create sumOfArray.test.js

---
+++ Solutions/sumOfArray.test.js
@@ -0,0 +1,21 @@
+const sumArray = require('./index')
+
+describe("testing sumArray function", () => {
+
+test( "empty array should give 0", () => {
+    expect(sumArray([])).toBe(0);
+});
+
+test( "add all elements of an input array", () => {
+    expect(sumArray([1,2,3])).toBe(6);
+});
+
+test( "array with one element should return the same element", () => {
+    expect(sumArray([10])).toBe(10);
+});
+
+test( "input is a string, should return undefined", () => {
+    expect(sumArray(["anna"])).toBe(NaN);
+});
+
+})
